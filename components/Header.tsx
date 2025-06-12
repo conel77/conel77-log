6abc1b90b8ac54b15d46e1e3570656f4b02426c0
신민경 <greenmk77@ajou.ac.kr>
2025-06-12 12:15:50 +0000

    Fix: 헤더 로고를 siteMetadata 기반 동적 이미지로 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
+import Image from 'next/image'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -17,7 +17,13 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Logo />
+            <Image
+              src={siteMetadata.siteLogo}
+              alt="site logo"
+              width={32}
+              height={32}
+              priority
+            />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
